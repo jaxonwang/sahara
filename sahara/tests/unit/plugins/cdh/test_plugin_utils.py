0b4701af35af2569e7e0d3ee4d11fe9526f9c432
Jaxon Wang <jiexingx.wang@intel.com>
2015-11-19 19:09:49 +0800

    Add unit test cases for cdh plugin utils
    For some methods implemented in the base class but depends on
    the versions, add tests in their own directory.
    
    changes:
    * add test cases for cdh base plugin utils
    * add test cases for cdh v5 plugin utils
    * add test cases for cdh v530 plugin utils
    * add test cases for cdh v540 plugin utils
    
    partially implements bp: cdh-plugin-refactoring
    
    Change-Id: If27fc0c51f66c596960eb031036cc411114f5b6f

--- sahara/tests/unit/plugins/cdh/test_plugin_utils.py
+++ sahara/tests/unit/plugins/cdh/test_plugin_utils.py
@@ -15,9 +15,10 @@
 
 import mock
 
+from sahara import config as cfg
 from sahara.plugins.cdh import plugin_utils as pu
 from sahara.tests.unit import base as b
-
+from sahara.tests.unit.plugins.cdh import utils as ctu
 
 CONFIGURATION_SCHEMA = {
     'node_configs': {
@@ -48,15 +49,102 @@ CONFIGURATION_SCHEMA = {
 }
 
 
+def get_concrete_cluster():
+    cluster = ctu.get_fake_cluster()
+
+    # add configs to cluster
+    configs = {"SQOOP": {}, "HUE": {}, "general": {}, "KMS": {}, "HIVE": {},
+               "SOLR": {}, "FLUME": {}, "HDFS": {"dfs_replication": 1},
+               "KS_INDEXER": {}, "SPARK_ON_YARN": {}, "SENTRY": {}, "YARN": {},
+               "ZOOKEEPER": {}, "OOZIE": {}, "HBASE": {}, "IMPALA": {}}
+    dict.__setitem__(cluster, "cluster_config", configs)
+
+    # add fake remotes to instances
+    instances = [i for ng in cluster.node_groups for i in ng.instances]
+    for i in instances:
+        object.__setattr__(i, 'remote', mock.MagicMock())
+
+    # add cluster_id to each node group
+    for ng in cluster.node_groups:
+        dict.__setitem__(ng, "cluster_id", ng.cluster.id)
+
+    # add extra config
+    dict.__setitem__(cluster, "extra", {})
+    return cluster
+
+
+def get_fake_worker_instances():
+    ng = get_concrete_cluster().node_groups[2]
+    # node group is immutable, a work around
+    return ng.instances
+
+
 class TestPluginUtils(b.SaharaTestCase):
+
+    def setUp(self):
+        super(TestPluginUtils, self).setUp()
+        self.plug_utils = pu.AbstractPluginUtils()
+        CONF = cfg.CONF
+        CONF.disable_event_log = True
+
     @mock.patch('sahara.plugins.cdh.plugin_utils.'
                 'CDHPluginAutoConfigsProvider')
     def test_recommend_configs(self, provider):
-        plug_utils = pu.AbstractPluginUtils()
         fake_plugin_utils = mock.Mock()
         fake_cluster = mock.Mock()
-        plug_utils.recommend_configs(fake_cluster, fake_plugin_utils, False)
-        self.assertEqual([
-            mock.call(CONFIGURATION_SCHEMA,
-                      fake_plugin_utils, fake_cluster, False)
-        ], provider.call_args_list)
+        self.plug_utils.recommend_configs(
+            fake_cluster, fake_plugin_utils, False)
+        self.assertEqual([mock.call(CONFIGURATION_SCHEMA,
+                                    fake_plugin_utils,
+                                    fake_cluster,
+                                    False)],
+                         provider.call_args_list)
+
+    @mock.patch('sahara.plugins.cdh.commands.install_packages')
+    def test_install_packages(self, install_packages):
+        packages = mock.Mock()
+        instances = get_fake_worker_instances()
+        self.plug_utils.install_packages(instances, packages)
+
+        calls = [mock.call(i.remote().__enter__(), packages)
+                 for i in instances]
+
+        install_packages.assert_has_calls(calls, any_order=False)
+
+    @mock.patch('sahara.plugins.cdh.commands.start_agent')
+    @mock.patch('sahara.plugins.cdh.commands.configure_agent')
+    def test_start_cloudera_agents(self,
+                                   configure_agent, start_agent):
+        instances = get_fake_worker_instances()
+
+        self.plug_utils.start_cloudera_agents(instances)
+
+        cfg_calls = [mock.call(i.remote().__enter__(), 'manager_inst')
+                     for i in instances]
+        start_calls = [mock.call(i.remote().__enter__()) for i in instances]
+
+        configure_agent.assert_has_calls(cfg_calls, any_order=False)
+        start_agent.assert_has_calls(start_calls, any_order=False)
+
+    def test_put_hive_hdfs_xml(self):
+        cluster = get_concrete_cluster()
+        hive_server = cluster.node_groups[1].instances[0]
+        self.plug_utils.put_hive_hdfs_xml(cluster)
+        with hive_server.remote() as r:
+            calls = [mock.call('sudo su - -c "hadoop fs -mkdir -p'
+                               ' /user/hdfs/conf" hdfs'),
+                     mock.call('sudo su - -c "hadoop fs -put'
+                               ' /etc/hive/conf/hive-site.xml'
+                               ' /user/hdfs/conf/hive-site.xml" hdfs')]
+            r.execute_command.assert_has_calls(calls, any_order=False)
+
+    def test_create_hive_hive_directory(self):
+        cluster = get_concrete_cluster()
+        namenode = cluster.node_groups[1].instances[0]
+        self.plug_utils.create_hive_hive_directory(cluster)
+        with namenode.remote() as r:
+            calls = [mock.call('sudo su - -c "hadoop fs -mkdir -p'
+                               ' /tmp/hive-hive" hdfs'),
+                     mock.call('sudo su - -c "hadoop fs -chown hive'
+                               ' /tmp/hive-hive" hdfs')]
+            r.execute_command.assert_has_calls(calls, any_order=False)
---
+++ sahara/tests/unit/plugins/cdh/v5_3_0/test_plugin_utils_530.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2015 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+
+from sahara.plugins.cdh.v5_3_0 import plugin_utils as pu
+from sahara.tests.unit.plugins.cdh import test_plugin_utils
+from sahara.tests.unit.plugins.cdh.v5 import test_plugin_utils_5
+from sahara.utils import files
+
+
+class TestPluginUtilsV530(test_plugin_utils_5.TestPluginUtilsV5):
+
+    def setUp(self):
+        super(TestPluginUtilsV530, self).setUp()
+        self.plug_utils = pu.PluginUtilsV530()
+        self.version = "v5_3_0"
+
+    @mock.patch('uuid.uuid4')
+    @mock.patch('sahara.conductor.API.cluster_update')
+    @mock.patch('sahara.conductor.API.cluster_get')
+    def test_configure_sentry(self, cluster_get, cluster_update, uuid4):
+        cluster = test_plugin_utils.get_concrete_cluster()
+        manager = cluster.node_groups[0].instances[0]
+        cluster_get.return_value = cluster
+        db_password = 'a8f2939f-ff9f-4659-a333-abc012ee9b2d'
+        uuid4.return_value = db_password
+        create_db_script = files.get_file_text(
+            'plugins/cdh/{version}/resources/create_sentry_db.sql'
+                                .format(version=self.version))
+        create_db_script = create_db_script % db_password
+
+        self.plug_utils.configure_sentry(cluster)
+
+        with manager.remote() as r:
+            cmd_exe_sql = ('PGPASSWORD=$(sudo head -1'
+                           ' /var/lib/cloudera-scm-server-db/data/'
+                           'generated_password.txt) psql'
+                           ' -U cloudera-scm -h localhost -p 7432 -d scm -f'
+                           ' script_to_exec.sql')
+            cmd_clean = 'rm script_to_exec.sql'
+            self.assertEqual(create_db_script, r.write_file_to.call_args[0][1])
+            r.execute_command.assert_has_calls([mock.call(cmd_exe_sql),
+                                                mock.call(cmd_clean)])
